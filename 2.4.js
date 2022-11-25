edf3da5de9502f0595db204d31867b5b0f5c63f1
Céline V <celine.verreydt.pro@gmail.com>
2022-11-25 15:18:09 +0100

    while loop

--- 2.4.js
+++ 2.4.js
@@ -3,3 +3,15 @@ for (let i = 1; i <= 100; i += 1) { if (i%2 == 0) {
 } else console.log(i, i*3)
 }
 
+//or
+
+let i = 1
+while (i <= 100) { if
+  (i%2 == 0) {
+    console.log(i, i/2)
+  } else console.log(i, i*3);
+  i += 1;
+  if (i === 101) {
+    break;
+  }
+} 
