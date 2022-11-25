3f66da44a615f35196708f27e41c78d80ede258f
Céline V <celine.verreydt.pro@gmail.com>
2022-11-25 15:59:33 +0100

    3.0 you can(not) redo

--- 3.0.js
+++ 3.0.js
@@ -1,6 +1,6 @@
 let keller5 = ["Arno Volts", "Aurélien Mariaule", "Aurore Lemaire", "Benjamin Porsont", "Céline Verreydt", "Corentin Miserque", "Dominique Coppée", "Edouard de Romrée de Vichenet", "Hugo Goorickx", "Jonathan Manes", "Jonathan Bajoux", "Laura Wilhelmi", "Lysie Soyez", "Marnie Benalia", "Mathilde Cornelis", "Milo Bonnet", "Nadim El Nakadi", "Nathalie Vanden Abeele", "Nathalie Goffette", "Nour Everaert", "Pierre Mauriello", "Quentin Bource", "Virginie Dourson"]
-for (let elem of keller5) {
-    console.log(elem)
+for (let student of keller5) {
+    console.log(student)
 }
 
 
