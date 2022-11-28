79dee09c7613c88cb20f37dc966188ff79f35197
Céline V <celine.verreydt.pro@gmail.com>
2022-11-28 15:52:03 +0100

    3

---
+++ 3.1.js
@@ -0,0 +1,10 @@
+let arr1 = [1, 2, 3, 4, 5]
+
+let arr2 = [100, 101, 102]
+
+let sum = 0;
+
+ for (let value of arr1) {
+    sum += value
+ }
+
---
+++ 3.2.js
@@ -0,0 +1,7 @@
+let arr = [1, 2, 3, 4, 5]
+let sum = 0;
+for (let value of arr) {
+    0 += value;
+ }
+let average = (sum / arr.length) //uncaught syntax error: Invalid left-hand side in assignment
+
