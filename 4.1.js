cc1fe9a6939532b59b98eb082672117e511860f0
Céline V <celine.verreydt.pro@gmail.com>
2022-11-28 15:52:44 +0100

    4

---
+++ 4.1.js
@@ -0,0 +1,10 @@
+
+function calcSurface(a, b) {
+    return a*b;
+}
+
+let a = prompt("Enter the length of the rectangle");
+let b = prompt("Enter the width of the rectangle");
+let surface = calcSurface(a, b);
+console.log(surface)
+
